2a14bf0fc8098da42229304a11864e393a88d789
arata-t <taniguro.arata@gmail.com>
2021-03-28 10:24:25 +0900

    日本語化対応２

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+         validates :nickname, presence: true, length: { maximum: 6 }
+
+         with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'には全角文字を使用してください' } do
+          validates :last_name
+          validates :first_name
+         end
 end
