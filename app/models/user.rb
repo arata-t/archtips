e32e50fb113ebf8503447fb6c17097a98c2ed0f6
arata-t <taniguro.arata@gmail.com>
2021-03-29 21:32:24 +0900

    バリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
          validates :nickname, presence: true, length: { maximum: 6 }
-
+         validates :password, presence: true, format: { with: /[a-z\d]{6,}/i, message: 'は半角英数６文字以上としてください' }
          with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'には全角文字を使用してください' } do
           validates :last_name
           validates :first_name
