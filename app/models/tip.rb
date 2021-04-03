f24a55c0dbc9980182ffbc1b1e3e6c24910d643d
arata-t <taniguro.arata@gmail.com>
2021-04-03 13:01:26 +0900

    バリデーション設定

--- app/models/tip.rb
+++ app/models/tip.rb
@@ -1,2 +1,8 @@
 class Tip < ApplicationRecord
+  validates :title,       presence: true
+  validates :category_id, presence: true
+  validates :description, presence: true
+
+  belongs_to :user
 end
+
