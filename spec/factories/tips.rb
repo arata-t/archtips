523bd0bf314f4b3bd5fe95d9ae4d388257904e79
arata-t <taniguro.arata@gmail.com>
2021-04-05 22:42:51 +0900

    ファクトリーbot導入

--- spec/factories/tips.rb
+++ spec/factories/tips.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
   factory :tip do
-    
+    title            { Faker::Lorem.words }
+    category_id      { 2 }
+    description      { Faker::Lorem.sentence }
+    association :user
+
+    after(:build) do |message|
+      message.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
