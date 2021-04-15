43566941607cc843016e27abf1eb4c2a3b9f65ba
arata-t <taniguro.arata@gmail.com>
2021-04-15 22:59:47 +0900

    add search models/tip.rb

--- app/models/tip.rb
+++ app/models/tip.rb
@@ -7,4 +7,12 @@ class Tip < ApplicationRecord
 
   validates :title, :description,       presence: true
   validates :category_id, numericality: { other_than: 1 }
+
+  def self.search(search)
+    if search != ""
+      Tip.where('description LIKE(?)', "%#{search}%")
+    else
+      Tweet.all
+    end
+  end
 end
