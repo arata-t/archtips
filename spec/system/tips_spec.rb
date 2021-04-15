7ce4e8fc6dcab106047b390dc37b4915fcbaf0b3
arata-t <taniguro.arata@gmail.com>
2021-04-15 21:14:30 +0900

    add system_spec

--- spec/system/tips_spec.rb
+++ spec/system/tips_spec.rb
@@ -23,7 +23,6 @@ RSpec.describe '投稿する', type: :system do
       end.not_to change { Tip.count }
     end
   end
-
   context '投稿に成功した時' do
     it '投稿に成功し、トップページに投稿したタイトル、カテゴリー、説明が表示されていること' do
       # ログイン
@@ -46,7 +45,6 @@ RSpec.describe '投稿する', type: :system do
       expect(page).to have_content(Category.data[@tip.category_id - 1][:name])
       expect(page).to have_content(@tip.description)
     end
-
     it ' 画像を含めた投稿が成功し、トップページに投稿した画像が表示されていること ' do
       # ログイン
       visit new_user_session_path
@@ -69,7 +67,6 @@ RSpec.describe '投稿する', type: :system do
       expect(page).to have_content(@tip.description)
       expect(page).to have_selector('img')
     end
-
     it '画像を含めた投稿が成功したら、投稿したタイトル。カテゴリー・説明・画像が詳細ページに表示されること' do
       # ログイン
       visit new_user_session_path
@@ -130,6 +127,31 @@ RSpec.describe '投稿する', type: :system do
       expect(page).to have_content(Category.data[other_tip.category_id - 1][:name])
       expect(page).to have_content(other_tip.description)
       expect(page).to have_selector('img')
+    end
+
+    it '投稿をを正しく削除できる' do
+      # ログイン
+      visit new_user_session_path
+      fill_in 'user_email', with: @user.email
+      fill_in 'user_password', with: @user.password
+      find('input[type="submit"]').click
+      # 投稿
+      visit new_tip_path
+      other_tip = FactoryBot.create(:tip)
+      fill_in 'tip_title', with: other_tip.title
+      select Category.data[other_tip.category_id - 1][:name], from: 'tip_category_id'
+      image_path = Rails.root.join('public/images/test_image.png')
+      attach_file 'tip-image-main-img', image_path, make_visible: true
+      fill_in 'tip_description', with: other_tip.description
+      expect  do
+        find('input[type="submit"]').click
+      end.to change { Tip.count }.by(1)
+      # 詳細
+      click_on other_tip.title, match: :first
+      #削除
+      click_on '削除'
+      expect(current_path).to eq root_path
+      # expect(page).not_to have_content(@tip.title)
     end
   end
 end
