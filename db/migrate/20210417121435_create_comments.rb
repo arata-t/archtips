eb903df17703ca2218da3cad62a763d94c4465d1
arata-t <taniguro.arata@gmail.com>
2021-04-18 07:19:01 +0900

    テーブル修正

--- db/migrate/20210417121435_create_comments.rb
+++ db/migrate/20210417121435_create_comments.rb
@@ -2,7 +2,7 @@ class CreateComments < ActiveRecord::Migration[6.0]
   def change
     create_table :comments do |t|
       t.integer :user_id
-      t.integer :tweet_id
+      t.integer :tip_id
       t.text :comment
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -35,7 +35,7 @@ ActiveRecord::Schema.define(version: 2021_04_17_121435) do
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id"
-    t.integer "tweet_id"
+    t.integer "tip_id"
     t.text "comment"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
