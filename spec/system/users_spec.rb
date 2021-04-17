a658a74162bef76352dceea0fe01939660e0d39a
arata-t <taniguro.arata@gmail.com>
2021-04-17 17:24:34 +0900

    指摘修正　投稿後にマイページアクセスすると投稿した内容が表示される。

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -6,15 +6,26 @@ RSpec.describe 'Users', type: :system do
     @user[:id] = @user.id + 1
     @tip = FactoryBot.create(:tip)
   end
+  
   context 'マイページにアクセスする' do
-    it 'ログインした状態でマイページアクセスするとニックネーム、自己紹介、投稿数、投稿一覧が表示される。' do
+    it '投稿後にマイページアクセスすると投稿した内容が表示される。' do
       # ログイン
       visit new_user_session_path
       fill_in 'user_email', with: @user.email
       fill_in 'user_password', with: @user.password
       find('input[type="submit"]').click
+      # 投稿
+      visit new_tip_path
+      fill_in 'tip_title', with: @tip.title
+      select Category.data[@tip.category_id - 1][:name], from: 'tip_category_id'
+      image_path = Rails.root.join('public/images/test_image.png')
+      attach_file 'tip-image-main-img', image_path, make_visible: true
+      fill_in 'tip_description', with: @tip.description
+      expect  do
+        find('input[type="submit"]').click
+      end.to change { Tip.count }.by(1)
       # マイページ
-      visit user_path(@user[:id])
+      click_on('マイページ')
       expect(page).to have_content(@user.nickname)
       expect(page).to have_content(@tip.description)
       expect(page).to have_content(@user.tips.length)
@@ -39,6 +50,7 @@ RSpec.describe 'Users', type: :system do
     it '詳細ページの投稿者を選択すると投稿者のマイページに移動し、投稿者のニックネーム、自己紹介、投稿数、投稿一覧が表示される。' do
       visit tip_path(@tip.id)
       click_on @user.nickname
+      # マイページ
       expect(current_path).to eq user_path(@user.id)
       expect(page).to have_content(@user.nickname)
       expect(page).to have_content(@tip.description)
