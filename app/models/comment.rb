906105c00a2f76d661288ac6738fc78e446b659b
arata-t <taniguro.arata@gmail.com>
2021-04-18 22:05:45 +0900

    rubocop

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,5 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :tip
 
-  validates :comment,       presence: true
+  validates :comment, presence: true
 end
--- spec/system/comments_spec.rb
+++ spec/system/comments_spec.rb
@@ -1,44 +1,44 @@
 require 'rails_helper'
 
-RSpec.describe "Comments", type: :system do
+RSpec.describe 'Comments', type: :system do
   before do
     @tip = FactoryBot.create(:tip)
     @comment = FactoryBot.create(:comment)
   end
 
   it 'コメントが投稿できること' do
-    #ログイン
+    # ログイン
     visit new_user_session_path
     fill_in 'user_email', with: @tip.user.email
     fill_in 'user_password', with: @tip.user.password
     find('input[type="submit"]').click
     expect(current_path).to eq(root_path)
-    #詳細
+    # 詳細
     click_on @tip.title
-    #コメント
+    # コメント
     fill_in 'comment_comment', with: @comment.comment
-    expect{
+    expect  do
       find('input[name="commit"]').click
-    }.to change { Comment.count }.by(1)
+    end.to change { Comment.count }.by(1)
     expect(current_path).to eq tip_path(@tip)
     expect(page).to have_content @comment.user.nickname
     expect(page).to have_content @comment.comment
   end
   it 'コメントが投稿できたらマイページのコメント数が変更していること' do
-    #ログイン
+    # ログイン
     visit new_user_session_path
     fill_in 'user_email', with: @tip.user.email
     fill_in 'user_password', with: @tip.user.password
     find('input[type="submit"]').click
     expect(current_path).to eq(root_path)
-    #詳細
+    # 詳細
     click_on @tip.title
-    #コメント
+    # コメント
     fill_in 'comment_comment', with: @comment.comment
-    expect{
+    expect  do
       find('input[name="commit"]').click
-    }.to change { Comment.count }.by(1)
-    #マイページ
+    end.to change { Comment.count }.by(1)
+    # マイページ
     visit user_path(@tip.user.id)
     expect(page).to have_content(@tip.user.comments.length)
   end
