966e8efde435bd90749906a43e6975819f17f4ca
arata-t <taniguro.arata@gmail.com>
2021-04-18 10:53:36 +0900

    comments_controller修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,11 @@
 class CommentsController < ApplicationController
   def create
     comment = Comment.create(comment_params)
-    redirect_to "/tip/#{comment.tip.id}"
+    redirect_to "/tips/#{comment.tip.id}"
   end
 
   private
   def comment_params
-    params.require(:comment).permit(:text).merge(user_id: current_user.id, tip_id: params[:tip_id])
+    params.require(:comment).permit(:comment).merge(user_id: current_user.id, tip_id: params[:tip_id])
   end
 end
