07e149c1d976f5289bd715860bbc4afc1d203ec0
arata-t <taniguro.arata@gmail.com>
2021-04-18 21:09:40 +0900

    コメントのコントローラーテスト実装

--- spec/requests/comments_spec.rb
+++ spec/requests/comments_spec.rb
@@ -1,7 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe "Comments", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  before do
+    @user = FactoryBot.create(:user)
+    @tip = FactoryBot.create(:tip)
+  end
+  describe "GET/tips/show" do
+    it "ログインした状態でtips/showにアクセスするとコメントが投稿覧が表示される" do
+      sign_in @user
+      get tip_path(@tip)
+      expect(response.body).to include 'コメントする'
+    end
+    it "ログインしていない状態でtips/showにアクセスするとコメント欄が表示されない" do
+      get tip_path(@tip)
+      expect(response.body).not_to include 'コメントする'
+    end
   end
 end
