9759bcd0bcfac5db3987c0fe6f4ab42f94705fc6
arata-t <taniguro.arata@gmail.com>
2021-04-24 19:27:20 +0900

    comments_suupport.rb　実装

--- spec/system/comments_spec.rb
+++ spec/system/comments_spec.rb
@@ -12,13 +12,7 @@ RSpec.describe 'Comments', type: :system do
     # 詳細
     click_on @tip.title
     # コメント
-    fill_in 'comment_comment', with: @comment.comment
-    expect  do
-      find('input[name="commit"]').click
-    end.to change { Comment.count }.by(1)
-    expect(current_path).to eq tip_path(@tip)
-    expect(page).to have_content @comment.user.nickname
-    expect(page).to have_content @comment.comment
+    comment(@comment)
   end
   it 'コメントが投稿できたらマイページのコメント数が変更していること' do
     # ログイン
@@ -26,10 +20,7 @@ RSpec.describe 'Comments', type: :system do
     # 詳細
     click_on @tip.title
     # コメント
-    fill_in 'comment_comment', with: @comment.comment
-    expect  do
-      find('input[name="commit"]').click
-    end.to change { Comment.count }.by(1)
+    comment(@comment)
     # マイページ
     visit user_path(@tip.user.id)
     expect(page).to have_content(@tip.user.comments.length)
