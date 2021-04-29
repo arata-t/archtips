fff84dbe291dc8b807d9836ac70f59ca44c8eb8a
arata-t <taniguro.arata@gmail.com>
2021-04-29 20:33:07 +0900

    tag.rb作成

---
+++ app/models/tip_tag.rb
@@ -0,0 +1,20 @@
+class TipTag
+
+  include ActiveModel::Model
+  attr_accessor :title, :category_id, :description, :user_id, :name
+
+  with_options presence: true do
+    validates :title
+    validates :description
+    validates :name
+    validates :category_id, numericality: { other_than: 1 }
+  end
+
+  def save
+    tip = Tip.create(title: title, category_id: category_id, description: description)
+    tag = Tag.create(name: name)
+
+    TipTagRelation.create(tip_id: tip.id, tag_id: tag.id)
+  end
+end
+
