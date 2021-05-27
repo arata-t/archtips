3da313505dda4296c77a54de9cd298f5f13ca744
arata-t <taniguro.arata@gmail.com>
2021-05-28 08:32:34 +0900

    マイページ　いいね表示追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,5 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @tips = User.find(params[:id]).tips.order(updated_at: :DESC)
-    num = 0
-    @tips.each do |tip|
-      num += tip.comments.length
-    end
-    @commented_num = num
   end
 end
