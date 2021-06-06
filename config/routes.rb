e67695a5d2301440f55ccff081f821cc439d1178
arata-t <taniguro.arata@gmail.com>
2021-06-06 20:35:17 +0900

    編集ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
+  resources :blogs
+
   root to: 'tips#index'
   resources :users, only: :show
   resources :tips do
