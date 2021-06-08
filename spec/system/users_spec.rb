234fe58eb72b88a1b673d217d2d5c54a530f9f87
arata-t <taniguro.arata@gmail.com>
2021-06-08 22:22:38 +0900

    users_spec　実装

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -66,4 +66,52 @@ RSpec.describe 'Users', type: :system do
       mypage(@tip)
     end
   end
+
+  context 'ユーザーを編集する' do
+    it 'ユーザーの編集を行うとトップページの名前が修正されている' do
+      # ログイン
+      sign_in(@tip.user)
+      # マイページ
+      click_on @tip.user.nickname, match: :first
+      # ユーザー編集
+      find('.btn-edit-str').click
+      fill_in 'user_nickname', with: '-c'
+      fill_in 'user_introduction', with: '-c'
+      find('input[type="submit"]').click
+      expect(page).to have_css ".user-button", text: '-c'
+    end
+    it 'ユーザー編集を行うとマイページの内容が修正されている' do
+      # ログイン
+      sign_in(@tip.user)
+      # マイページ
+      click_on @tip.user.nickname, match: :first
+      # ユーザー編集
+      find('.btn-edit-str').click
+      fill_in 'user_nickname', with: '-c'
+      fill_in 'user_introduction', with: '-c'
+      find('input[type="submit"]').click
+      expect(page).to have_css ".user-button", text: '-c'
+      # マイページ編集
+      find('#my-page-btn').click
+      expect(page).to have_css ".plofile-name", text: '-c'
+      expect(page).to have_css ".plofile-introduction", text: '-c'
+    end
+  end
+
+  context '退会する' do
+    it '退会に成功し、退会したユーザーに付随した投稿も削除される' do
+      # ログイン
+      sign_in(@tip.user)
+      # マイページ
+      click_on @tip.user.nickname, match: :first
+      # 退会
+      find('.btn-delete-str').click
+      expect(page.driver.browser.switch_to.alert.text).to eq "本当に退会しますか？"
+      page.driver.browser.switch_to.alert.accept
+      expect(page).to have_css ".new-user-button", text: 'ユーザー登録'
+      expect(page).not_to have_content @tip.title
+      expect(page).not_to have_content @tip.tags
+      expect(page).not_to have_content @tip.category
+    end
+  end
 end
