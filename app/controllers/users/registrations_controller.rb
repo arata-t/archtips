28288be5227bd71399f668fe5f05f04b502875df
arata-t <taniguro.arata@gmail.com>
2021-06-08 22:24:14 +0900

    コントローラー修正（仮）

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,4 +1,5 @@
 class Users::RegistrationsController < Devise::RegistrationsController
+  before_action :authenticate_user!
   before_action :configure_account_update_params, only: [:update]
 
   protected
