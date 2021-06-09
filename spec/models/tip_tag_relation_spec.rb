c85a021901900ed7e7a57852bb58ebfdb2e48deb
arata-t <taniguro.arata@gmail.com>
2021-06-09 23:22:23 +0900

    molel tip_tag_relations 修正・追記

--- spec/models/tip_tag_relation_spec.rb
+++ spec/models/tip_tag_relation_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe TipTagRelation, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @tip_tag_relation = FactoryBot.create(:tip_tag_relation)
+  end
+
+  context '正常系' do
+    it 'tip_idとtag_idがあれば正常' do
+      expect(@tip_tag_relation).to be_valid
+    end
+    it 'tag_idが同じでもtip_idが異なればあれば正常' do
+      tip_tag_relation = FactoryBot.create(:tip_tag_relation)
+      expect(FactoryBot.create(:tip_tag_relation, tag_id:tip_tag_relation.tag_id )).to be_valid
+    end
+    it 'tip_idが同じでもtag_idが異なればあれば正常' do
+      tip_tag_relation = FactoryBot.create(:tip_tag_relation)
+      expect(FactoryBot.create(:tip_tag_relation, tip_id:tip_tag_relation.tip_id )).to be_valid
+    end
+  end
+  context '異常系' do
+    it 'tip_idがなければ無効な状態であること' do
+      @tip_tag_relation.tip_id = nil
+      @tip_tag_relation.valid?
+      expect(@tip_tag_relation.errors.full_messages).to include "Tip must exist"
+    end
+    it 'tag_idがなければ無効な状態であること' do
+      @tip_tag_relation.tag_id = nil
+      @tip_tag_relation.valid?
+      expect(@tip_tag_relation.errors.full_messages).to include "Tag must exist"
+    end
+  end
 end
