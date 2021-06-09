9fb894544a7d938432a2ae7d8d2896054264b435
arata-t <taniguro.arata@gmail.com>
2021-06-09 23:18:09 +0900

    model/tip_tag_relation.rb バリデーション追加

--- app/models/tip_tag_relation.rb
+++ app/models/tip_tag_relation.rb
@@ -1,4 +1,7 @@
 class TipTagRelation < ApplicationRecord
   belongs_to :tip
   belongs_to :tag
+
+  validates :tip_id, presence:true
+  validates :tag_id, uniqueness: { scope: :tip_id }, presence: true
 end
