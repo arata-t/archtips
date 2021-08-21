28870bf1ef950302d70ac7cd0698d416a60d59e6
arata-t <76896195+arata-t@users.noreply.github.com>
2021-08-21 10:55:43 +0900

    detail.js  コード修正

--- app/javascript/detail_search.js
+++ app/javascript/detail_search.js
@@ -3,25 +3,6 @@ window.addEventListener('load', function(){
   const searchBtn   = document.getElementById("search-btn")
   const visibleTrue = document.getElementById("detail-search-area")
   const searchArea  = document.getElementById("search")
-  // 新規投稿
-  const newTipArea  = document.getElementById("new-tip")
-  const newTipBtn   = document.getElementById("new-tip-btn")
-  // トレンド
-  const newTrendArea = document.getElementById("new-trend")
-  const newTrendBtn  = document.getElementById("trend-btn")
-  // マイページ
-  const myPageArea = document.getElementById("my-page")
-  const myPageBtn  = document.getElementById("my-page-btn")
-  
-  // 詳細検索の時はサイドバーの検索をON、バックグラウンド
-  const path = location.pathname ;
-  if (path == "/tips/detail_search"){
-    visibleTrue.getAttribute("style")
-    visibleTrue.setAttribute("style", "display:block;")
-    searchArea.setAttribute("style", "background-color:rgb(230, 230, 230);")
-  }
-
-  // 詳細検索
   searchBtn.addEventListener('mouseover', function(){
     searchArea.setAttribute("style", "background-color:rgb(230, 230, 230);")
   })
@@ -39,25 +20,42 @@ window.addEventListener('load', function(){
       searchArea.setAttribute("style", "background-color:rgb(230, 230, 230);")
     }
   })
+  
   // 新規投稿
+  const newTipArea  = document.getElementById("new-tip")
+  const newTipBtn   = document.getElementById("new-tip-btn")
   newTipBtn.addEventListener('mouseover', function(){
     newTipArea.setAttribute("style", "background-color:rgb(230, 230, 230);")
   })
   newTipBtn.addEventListener("mouseout", function(){
     newTipArea.removeAttribute("style", "background-color:rgb(230, 230, 230);")
   })
-  // 質問する
+
+  // トレンド
+  const newTrendArea = document.getElementById("new-trend")
+  const newTrendBtn  = document.getElementById("trend-btn")
   newTrendBtn.addEventListener('mouseover', function(){
     newTrendArea.setAttribute("style", "background-color:rgb(230, 230, 230);")
   })
   newTrendBtn.addEventListener("mouseout", function(){
     newTrendArea.removeAttribute("style", "background-color:rgb(230, 230, 230);")
   })
+
   // マイページ
+  const myPageArea = document.getElementById("my-page")
+  const myPageBtn  = document.getElementById("my-page-btn")
   myPageBtn.addEventListener('mouseover', function(){
     myPageArea.setAttribute("style", "background-color:rgb(230, 230, 230);")
   })
   myPageBtn.addEventListener("mouseout", function(){
     myPageArea.removeAttribute("style", "background-color:rgb(230, 230, 230);")
   })
+  
+  // 詳細検索の時はサイドバーの検索をON、バックグラウンド
+  const path = location.pathname ;
+  if (path == "/tips/detail_search"){
+    visibleTrue.getAttribute("style")
+    visibleTrue.setAttribute("style", "display:block;")
+    searchArea.setAttribute("style", "background-color:rgb(230, 230, 230);")
+  }
 })
